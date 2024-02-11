14151fd29204cd232d80b69e588922face67651d
Gabriel Fernandez <gfernandez@mailfence.com>
2024-02-10 23:42:11 -0500

    Forgot to import the new config like a fool. Fixed.

--- hosts/turbine/configuration.nix
+++ hosts/turbine/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./../../modules/nixos/sddm.nix
       inputs.home-manager.nixosModules.default
       inputs.sddm-catppuccin.packages.${pkgs.hostPlatform.system}.sddm-catppuccin
     ];
