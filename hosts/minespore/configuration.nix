c0de81f0e0246cae020695a03697171e546b6800
Gabriel Fernandez <gfernandez@mailfence.com>
2024-03-17 19:34:10 -0400

    Hopefully added virt copy past

--- hosts/minespore/configuration.nix
+++ hosts/minespore/configuration.nix
@@ -5,7 +5,7 @@
 { config, pkgs, inputs, lib, ... }:
 
 {
-  imports =
+    imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./../../modules/nix/samba.nix
@@ -111,8 +111,9 @@
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "23.11"; # Did you read the comment?
+    services.qemuGuest.enable = true;
 }
