ccababa4b927c70974b84883ada33848067585d2
Gabriel Fernandez <gfernandez@mailfence.com>
2024-03-18 12:16:06 -0400

    Changed rcon password and server difficulty

--- modules/nix/minecraft.nix
+++ modules/nix/minecraft.nix
@@ -27,6 +27,8 @@ in {
                     spawn-protection = 0;
                     max-tick-time = 5 * 60 * 1000;
                     allow-flight = true;
+                    difficulty = "normal";
+                    rcon-password = "TheThrongler";
                 };
             };
         };
